0cb8930479ebb2773b9613d3929af31578dd5d80
Pedro Contreras Avendano <45802239+pcavendano@users.noreply.github.com>
2023-08-23 15:53:41 -0400

    Add components folder

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+      <h1>Pedro Contreras Avendano</h1>
+      <img
+        src="https://i.imgur.com/yXOvdOSs.jpg"
+        alt="Pedro COntreras Avendano"
+        className="photo"
+      />
+      <ul>
+        <li>Invent new traffic lights</li>
+        <li>Rehearse a movie scene</li>
+        <li>Improve spectrum technology</li>
+      </ul>
+    </>
+  )
+}
+
+export default Navbar
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import About from "./About";
+import Contact from "./Contact";
+import Projects from "./Projects";
+
+export { Navbar, Hero, About, Contact, Projects };
