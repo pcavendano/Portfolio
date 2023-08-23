c1a3673ff25bd7991d8d4829556189275f9469b3
Pedro Contreras Avendano <45802239+pcavendano@users.noreply.github.com>
2023-08-23 15:53:16 -0400

    Add components to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import styles from './styles'
+import { Navbar, Hero, About, Contact } from './components'
 
+//if your React component only returns JSX, you can use an arrow function
+//without the return statement and without the curly braces
 const App = () => {
   return (
-    <div>App</div>
+    <div className='bg-primary w-full overflow-hidden '>
+      <h1>Pedro Contreras Avendano</h1>
+      <Navbar />
+      <Hero />
+      <About />
+      <Contact />
+    </div>
   )
 }
 
