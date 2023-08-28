82f7e9fb1343a3eceb10d49048c4bd2e6b977007
Pedro Contreras Avendano <45802239+pcavendano@users.noreply.github.com>
2023-08-28 10:13:10 -0400

    Add lootie flame animation to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,51 @@
-import React from 'react'
+import React from "react";
+import Lottie from "lottie-react";
+import fire from "../assets/lottie/fire.json";
 
 const Navbar = () => {
-  return (
-    <>
-      <h1>Pedro Contreras Avendano</h1>
-      <img
-        src="https://i.imgur.com/yXOvdOSs.jpg"
-        alt="Pedro COntreras Avendano"
-        className="photo"
-      />
-      <ul>
-        <li>Invent new traffic lights</li>
-        <li>Rehearse a movie scene</li>
-        <li>Improve spectrum technology</li>
-      </ul>
-    </>
-  )
-}
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: fire,
+        rendererSettings: {
+            preserveAspectRatio: "xMidYMid slice",
+        },
+    };
 
-export default Navbar
+    return (
+        <header className="site-header">
+            <nav>
+                <div className="site-logo">
+                    <a className="fullname" href="/">
+                        <span className="firstName">Pedro</span>
+                        <span className="lastName">Contreras</span>
+                        <span className="lastName">Avendano</span>
+                    </a>
+                    <span className="profession">Full Stack Web Developer</span>
+                </div>
+                <ul>
+                    <li>
+                        <div className="decorative-space">
+                            <a href="/about">About</a>
+                        </div>
+                    </li>
+                    <li>
+                        <a href="/projects">Projects</a>
+                    </li>
+                    <li>
+                        <a className="hireme" href="/" target="_blank">
+                            <span>Hire Me</span>
+                            <Lottie
+                                className="fire"
+                                animationData={fire}
+                                options={defaultOptions}
+                            />
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Navbar;
