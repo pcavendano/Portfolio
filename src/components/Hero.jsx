e70bca44847e88538d177350a30245d1eeed5ff8
Pedro Contreras Avendano <45802239+pcavendano@users.noreply.github.com>
2023-08-28 10:12:41 -0400

    Beginning of Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
-
+import pedro from '../assets/pedro5.jpg'
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='home_hero'>
+      <div className='ctn'>
+      <figure>
+        <img src={pedro} alt="photo de profil" ></img>
+      </figure>
+      <div className='content'>
+        <h1>
+        <span>Full Stack Web Developer</span>
+        <br/>
+        <span>Open To Opportunities</span>
+        </h1>
+        <p>Future professional web developper building interactive web products - ready to commit, learn, adapt and improve.
+        I develop digital products from scratch
+        </p>
+      </div>
+      </div>
+    </section>
   )
 }
 
