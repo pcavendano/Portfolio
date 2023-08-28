9557897999f26221a48427b135041c5dc837ef0d
Pedro Contreras Avendano <45802239+pcavendano@users.noreply.github.com>
2023-08-28 10:16:58 -0400

    move navbaroutside of main

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-/* eslint-disable no-unused-vars */
-import React from 'react'
-import styles from './styles'
-import { Navbar, Hero, About, Contact } from './components'
+import React from "react";
+import { Navbar, Hero, About, Contact } from "./components";
 
-//if your React component only returns JSX, you can use an arrow function
-//without the return statement and without the curly braces
+
 const App = () => {
-  return (
-    <div className='bg-primary w-full overflow-hidden '>
-      <h1>Pedro Contreras Avendano</h1>
-      <Navbar />
-      <Hero />
-      <About />
-      <Contact />
-    </div>
-  )
-}
 
-export default App
+    return (
+        <div id="site">
+          <div id="page" className="home">
+            <Navbar />
+            <main>
+              <Hero/>
+              <About />
+              <Contact />
+            </main>
+            <section className="site-footer"></section>
+          </div>
+        </div>
+    );
+};
+
+export default App;
